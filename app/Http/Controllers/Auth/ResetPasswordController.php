8e4200deaa390790c5aa719f387ee63bc9e2fbc1
vipinyadav10 <vipin.y@iwebsun.com>
2018-12-05 12:16:24 +0530

    reset password redirect to changed to /

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/account';
+    protected $redirectTo = '/';
 
     /**
      * Get the password reset validation rules.

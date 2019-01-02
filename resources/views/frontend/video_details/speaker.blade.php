df4743e4b8fb7f1bd57011c329ce651057987d88
vipinyadav10 <vipin.y@iwebsun.com>
2019-01-02 17:26:27 +0530

    spekaer title

--- resources/views/frontend/video_details/speaker.blade.php
+++ resources/views/frontend/video_details/speaker.blade.php
@@ -2,21 +2,21 @@
 	<ul class="video-speaker-profile">
 		@foreach( $speakers as $speaker )
 		  	<li> 
-		        <a href="{{ url( 'speaker/'.$speaker['slug'] ) }}" class="profile-pic float-left">
+		        <a href="{{ url( 'speaker/'.$speaker['slug'] ) }}" class="profile-pic float-left" title="{{ $speaker['name'] }} ">
 		        	@if( !empty( $speaker['avatar'] ) )
 		        		<img src="{{ asset( $speaker['avatar'] ) }}" alt="{{ $speaker['name'] }} " title="{{ $speaker['name'] }} " />
 		        	@else
-		              	<img src="{{ asset( 'frontend/images/tmm-logo2.png' ) }}"alt="{{ $speaker['name'] }} " title="{{ $speaker['name'] }} ">
+		              	<img src="{{ asset( 'frontend/images/tmm-logo2.png' ) }}" alt="{{ $speaker['name'] }} " title="{{ $speaker['name'] }} ">
 		          @endif
 		        </a>
 		        <a href="{{ url( 'speaker/'.$speaker['slug'] ) }}" class="title" title="{{ $speaker['name'] }} ">
 		        	{{ ucfirst( $speaker['name'] ) }} 
 		        </a>
-		        <div class="designation">
+		        <div class="designation" title="Speaker Designation">
  		        	{{ ucfirst( $speaker['designation'] ) }}
 		        </div>
 		        <p>
-		        	{{ $speaker['short_description'] }}
+		        	{{ $speaker['biography'] }}
 		        </p>
 		    </li>
 		@endforeach

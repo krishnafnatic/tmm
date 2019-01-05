113a379cb52fa34096ba74a0b0bb790fb48f1c22
vipinyadav10 <vipin.y@iwebsun.com>
2019-01-05 17:19:46 +0530

    about title tag

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -6,9 +6,9 @@
 		<div class="col-sm-6">
 		 	<!--<iframe width="100%" height="100%" src="https://www.youtube.com/embed/9prd9nCNQXI?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>-->
 		 	<video width="100%" height="100%" controls>
-  <source src="{{ asset( 'frontend/video/WHATSAP_MONEYMILE_PROMO.mp4' ) }}" type="video/mp4">
-Your browser does not support the video tag.
-</video>
+        <source src="{{ asset( 'frontend/video/WHATSAP_MONEYMILE_PROMO.mp4' ) }}" type="video/mp4">
+      Your browser does not support the video tag.
+      </video>
 		</div>
 	 	<div class="col-sm-1">
 	 		<div class="vl"> </div>
@@ -18,19 +18,19 @@ Your browser does not support the video tag.
 		 	<br>
 		 	<div class="row">
 			 	<div class="col-sm-4 text-center p-0">
-			 		<img src="{{ asset( 'frontend/images/tmm-logo2.png' ) }}" class="tmm-profile-logo ">
+			 		<img src="{{ asset( 'frontend/images/tmm-logo2.png' ) }}" class="tmm-profile-logo" title="{{ config('app.name', 'TheMoneyMile') }}" alt="{{ config('app.name', 'TheMoneyMile') }}">
 			 	</div>
 		 	
                 <div class="col-sm-8 text-center"><br>
-                	<h3>Money Mile</h3>     
+                	<h3 title="Website Title">The Money Mile</h3>     
                  	<ul class="user-profile-social-ico">
                    		<li>
-                   			<a href="https://www.facebook.com/TheMoneyMile/" target="_blank">
+                   			<a href="https://www.facebook.com/TheMoneyMile/" target="_blank" title="Follow on Linkedin" alt="Follow on Linkedin">
                    				<i class="fab fa-facebook fa-2x"></i>
                    			</a>
                    		</li>
                    		<li>
-                   			<a href="https://www.linkedin.com/company/themoneymile/" target="_blank">
+                   			<a href="https://www.linkedin.com/company/themoneymile/" target="_blank" title="Follow on Linkedin" alt="Follow on Linkedin">
                    				<i class="fab fa-linkedin fa-2x"></i>
                    			</a>
                    		</li>
@@ -40,7 +40,7 @@ Your browser does not support the video tag.
                    			</a>
                    		</li> -->
                    		<li>
-                   			<a href="https://twitter.com/money_mile" target="_blank">
+                   			<a href="https://twitter.com/money_mile" target="_blank" title="Follow on Linkedin" alt="Follow on Twitter">
                    				<i class="fab fa-twitter-square fa-2x"></i>
                    			</a>
                    		</li>

85b5c277e403b37fdc03807cef6be17d8b644ef4
vipinyadav10 <vipin.y@iwebsun.com>
2019-01-15 16:38:32 +0530

    503+down

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,8 +1,12 @@
-@extends('errors::layout')
+@extends('frontend.layouts.down')
 
-@section('title', 'Website Under Maintenance Mode | The MoneyMile')
+@section('title')
 
-@section('content')
+	<title>503 - Website Under Maintenance | {{ config('app.name') }} | {{ config('app.message') }}</title>
+@stop
+
+@section('logo')
     <img src="{{ asset( 'frontend/images/the_money_mile_logo.png' ) }}" width="220" height="30" title="{{ config('app.name', 'The MoneyMile') }}" alt="{{ config('app.name', 'The MoneyMile') }}" />
 @stop
+
 @section('message', ' Website Under Maintenance. Be right back. ')

6aa7a97f46bd9609ae33d1358331b42edb086f84
vipinyadav10 <vipin.y@iwebsun.com>
2019-01-15 16:30:32 +0530

    503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,7 @@
 
 @section('title', 'Website Under Maintenance Mode | The MoneyMile')
 
+@section('content')
+    <img src="{{ asset( 'frontend/images/the_money_mile_logo.png' ) }}" width="220" height="30" title="{{ config('app.name', 'The MoneyMile') }}" alt="{{ config('app.name', 'The MoneyMile') }}" />
+@stop
 @section('message', ' Website Under Maintenance. Be right back. ')

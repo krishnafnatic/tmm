2d3f0400cfecd8b34b3e5f24c45dbee94ef88d7b
vipinyadav10 <vipin.y@iwebsun.com>
2019-01-15 17:33:02 +0530

    video

--- app/Http/Controllers/Frontend/VideoDetailController.php
+++ app/Http/Controllers/Frontend/VideoDetailController.php
@@ -660,7 +660,7 @@ class VideoDetailController extends Controller
         }
     } 
 
-    public function folderWatchNext( $category_name, $video_slug, VideoMeta $videoMeta ) {
+    public function folderWatchNext( urldecode ($category_name ), $video_slug, VideoMeta $videoMeta ) {
 
         $verifyVideoSlug = Video::where( 'slug', $video_slug )->first();
 

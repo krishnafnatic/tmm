54b7bbb06ac0bf470b51957fca5590b18b658ca4
vipinyadav10 <vipin.y@iwebsun.com>
2019-01-17 13:09:08 +0530

    env+meta

--- resources/views/frontend/elements/meta.blade.php
+++ resources/views/frontend/elements/meta.blade.php
@@ -31,5 +31,5 @@
   <!-- Google WebMaster-->
   <meta name="google-site-verification" content="AHrDaEmRXAy1RWs6NtDpq8z1llYurCKN4w_h1RcB2f4" />
   <!-- ReCaptcha-->
-  <script src='https://www.google.com/recaptcha/api.js?render=6LfufIoUAAAAAPMHlm8m-8xnR75xI5O5Zh5-uGnh'></script>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
